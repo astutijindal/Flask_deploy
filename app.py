b597fa96249a270ee40a8a4fbe47f3b09586218b
astutijindal <71984242+astutijindal@users.noreply.github.com>
2022-01-25 11:31:03 +0530

    Add files via upload
    First upload of files

---
+++ app.py
@@ -0,0 +1,31 @@
+from crypt import methods
+import numpy as np
+
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+##creating a flask app and naming it "app"
+app = Flask(__name__)
+model = pickle.load(open('model.pkl','rb'))
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+## model code
+
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final_features =[np.array(int_features)]    
+    
+    prediction = model.predict(final_features)
+
+    output= round(prediction[0],2)
+    return render_template('index.html', prediction_text='Employee Salary should be ${}'.format(output))
+
+if __name__ == '__main__':
+	app.run(debug=True)
